38d0c87636f691ff277d688651da316825673e1c
Soichi Sumi <soichi.sumi@gmail.com>
2019-02-28 16:11:05 +0900

    remove unnessesary codes

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/sendgrid/sendgrid-go"
 	"log"
 	"os"
 )
 
-// requert.Bodyに格納するJSONの元となるメールを表す構造体
 type Mail struct {
 	Subject          string             `json:"subject"`
 	Personalizations []Personalizations `json:"personalizations"`
@@ -16,34 +16,27 @@ type Mail struct {
 	Content          []Contents         `json:"content"`
 }
 
-// 封筒のようなもの
-// メールのメタデータを表す構造体
 type Personalizations struct {
 	To []MailUser `json:"to"`
 }
 
-// メールのユーザーを表す構造体
 type MailUser struct {
 	Email string `json:"email"`
 	Name  string `json:"name"`
 }
 
-// メールの中身を表す構造体
 type Contents struct {
 	Type  string `json:"type"`
 	Value string `json:"value"`
 }
 
-// .envファイルを読み込んで、ロードする
-func Env_load() {
-	// .envファイルを読み込んで、ロード
+func main() {
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Println(err)
 	}
-}
 
-func main() {
 	subject := "テスト"
 	contents := "テストですよ"
 
@@ -52,19 +45,11 @@ func main() {
 
 // メールの中身を作成して、メールを送信する
 func SendMail(subject, contents string) {
-
-	Env_load()
-
-	// .envファイルに格納したAPI KEYを取得
-	apiKey := os.Getenv("SENDGRID_API_KEY")
-	// ホスト
+	apiKey := os.Getenv("API_KEY")
 	host := "https://api.sendgrid.com"
-	// エンドポイント
 	endpoint := "/v3/mail/send"
 
-	// API KEYとエンドポイント、ホストからrestパッケージのRequestを生成
 	request := sendgrid.GetRequest(apiKey, endpoint, host)
-	// requestのMethodをPostに
 	request.Method = "POST"
 
 	// メールの内容をJSONで作成する
@@ -72,17 +57,17 @@ func SendMail(subject, contents string) {
 		Subject: subject,
 		Personalizations: []Personalizations{
 			{To: []MailUser{{
-				Email: os.Getenv("RECEIVER_USER_ADDRESS1"),
-				Name:  os.Getenv("RECEIVER_USER_NAME1"),
+				Email: os.Getenv("RECEIVER_ADDRESS1"),
+				Name:  os.Getenv("RECEIVER_NAME1"),
 			},
 				{
-					Email: os.Getenv("RECEIVER_USER_ADDRESS2"),
-					Name:  os.Getenv("RECEIVER_USER_NAME2"),
+					Email: os.Getenv("RECEIVER_ADDRESS2"),
+					Name:  os.Getenv("RECEIVER_NAME2"),
 				},
 			}}},
 		From: MailUser{
-			Email: os.Getenv("SENDER_USER_ADDRESS"),
-			Name:  os.Getenv("SENDER_USER_NAME"),
+			Email: os.Getenv("SENDER_ADDRESS"),
+			Name:  os.Getenv("SENDER_NAME"),
 		},
 		Content: []Contents{{
 			Type:  "text/plain",
@@ -90,7 +75,6 @@ func SendMail(subject, contents string) {
 		}},
 	}
 
-	// GoのコードをJSON化
 	data, err := json.Marshal(mail)
 
 	log.Println(string(data))
@@ -99,11 +83,8 @@ func SendMail(subject, contents string) {
 		log.Println(err)
 	}
 
-	// JSON化したmailの内容をrequest.Bodyに代入
 	request.Body = data
 
-	// sendGridのAPIにリクエストをセット
-	// 戻り値でresponseが返ってくる
 	response, err := sendgrid.API(request)
 	if err != nil {
 		log.Println(err)
